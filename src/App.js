d5339ac726b9963f7eeb487534730ba7b091607f
Rostysalv Kopachevskyi <waytojunioor@gmail.com>
2024-03-19 18:07:47 +0100

    counter implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentNum: 0
+    }
+    this.maxNum = 10
+    this.minNum = -10
+    this.defaultNum = 0
+  }
+
+  increaseNum = () => {
+    if(this.state.currentNum < this.maxNum) {
+      this.setState(state => ({
+        currentNum: state.currentNum + 1
+      }))
+    }
+  }
+
+  decreaseNum = () => {
+    if(this.state.currentNum > this.minNum) {
+      this.setState(state => ({
+        currentNum: state.currentNum - 1
+      }))
+    }
+  }
+
+  randomNum = () => {
+    let random = Math.floor(Math.random() * (this.maxNum - this.minNum + 1)) + this.minNum;
+    this.setState({
+      currentNum: random
+    })
+  }
+
+  resetNum = () => {
+    this.setState({
+      currentNum: this.defaultNum
+    })
+  }
+
+  render() {
+    return (
+      <div class="app">
+        <div class="counter">{this.state.currentNum}</div>
+        <div class="controls">
+          <button onClick={this.increaseNum}>INC</button>
+          <button onClick={this.decreaseNum}>DEC</button>
+          <button onClick={this.randomNum}>RND</button>
+          <button onClick={this.resetNum}>RESET</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
